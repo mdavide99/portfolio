e464153f67e26ca1f1fba5f3e18b73518a645889
Davide Marinotto <info@davidemarinotto.com>
2022-03-08 18:55:07 +0100

    add dotenv example

--- web/src/client.js
+++ web/src/client.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID,
+  projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-26', 
   useCdn: true 
